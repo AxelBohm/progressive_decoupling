e5f7d47dbf2393675fe84b26fabb5c7984b6b526
AxelBohm <axel.boehm@ymail.com>
2019-02-09 16:31:43 +0100

    changed imports

--- example_matrix_indefinite.py
+++ example_matrix_indefinite.py
@@ -1,9 +1,10 @@
 import numpy as np
 import numpy.linalg as la
 import matplotlib.pyplot as plt
+from scipy.linalg import block_diag
 
-from decoupling import decomposable_decoupling
-from util import CallBack, Quadratic, random_psd, projection_onto_linkage_space
+import decoupling as dc
+import util as ut
 
 import pdb
 
@@ -18,39 +19,35 @@ def main():
     nA = 4
 
     # construct PSD matrix
-    M = random_psd(n)
-    M_obj = Quadratic(M + np.eye(n))
+    M = ut.random_psd(n)
 
     # construct split
-    A = M.copy()
+    A = M.matrix.copy()
     A[nA:n, :] = 0
     A[:, nA:n] = 0
-    A = Quadratic(A)
-    C = M.copy()
+    A = ut.Quadratic(A)
+    C = M.matrix.copy()
     C[0:nA, :] = 0
     C[:, 0:nA] = 0
-    C = Quadratic(C)
-    B = M.copy()
+    C = ut.Quadratic(C)
+    B = M.matrix.copy()
     B[0:nA, 0:nA] = 0
     B[nA:n, nA:n] = 0
-    B = Quadratic(M)
+    B = ut.Quadratic(B)
 
     # construct projections
-    P = projection_onto_linkage_space(n, 3)
-    P_perp = np.eye(3*n) - P
+    P = ut.projection_onto_linkage_space(n, 3)
 
     # construct objective function in product space
     M_large = np.zeros((3*n, 3*n))
     M_large[:n, :n] = A.matrix
     M_large[n:2*n, n:2*n] = B.matrix
     M_large[2*n:, 2*n:] = C.matrix
+    M_large = block_diag(A.matrix, B.matrix, C.matrix)
+    M_large = ut.Quadratic(M_large)
 
-    alpha = M_obj.smallest_eigenvalue()
-    gamma = la.norm(la.multi_dot([P_perp, M_large, P_perp]), 2)
-    beta = 0.5*la.norm(la.multi_dot([P, M_large+M_large.T, P_perp]), 2)
-
-    e_0 = beta**2/alpha + gamma
-
+    e_0 = ut.compute_minimal_elicitation(M, M_large, P)
+    pdb.set_trace()
     # global minimizer
     x_opt = np.zeros(n)
 
@@ -62,10 +59,10 @@ def main():
     x0 = np.ones(n)
 
     def dist_to_solution(x): return la.norm(x - x_opt)
-    callback = CallBack(M_obj, dist_to_solution)
+    callback = ut.CallBack(M, dist_to_solution)
 
-    x_opt = decomposable_decoupling([A, C, B], niter, e, r, n, x0=x0,
-                                    callback=callback)
+    x_opt = dc.decomposable_decoupling([A, C, B], niter, e, r, n, x0=x0,
+                                       callback=callback)
 
     # make plots
     plt.plot(np.arange(niter), callback.stored_obj_fun_values)
